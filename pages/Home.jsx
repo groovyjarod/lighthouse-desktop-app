eac389536912f87fb5e4ce4c6e362e8699ce844d
groovyjarod <jarodd99@gmail.com>
2025-09-24 17:45:35 -0600

    updated to 1.1.2, testing if updates work for installed app

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <>
       <VStack {...CenteredVstackCss}>
         <h1>Lighthouse Automation Tool</h1>
-        <h3>Version 1.1.0</h3>
+        <h3>Version 1.1.2</h3>
         <h2>Created for FamilySearch</h2>
         <VStack width="100%" gap="16px">
           <LinkButton
--- pages/SettingsMenu.jsx
+++ pages/SettingsMenu.jsx
@@ -31,7 +31,7 @@ const SettingsMenu = () => {
 
   return (
     <VStack {...CenteredVstackCss}>
-        <MenuHeader title="Settings" subTitle="Version 1.0.15" />
+        <MenuHeader title="Settings" subTitle="Version 1.1.2" />
         <HStack {...CenteredHstackCss}>
           <VStack {...BodyVstackCss}>
             <h2>Change Paths</h2>
